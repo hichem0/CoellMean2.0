38b59cd7dfc9c9fcef533cfb4df97d632349344c
Nicolas Beaussart <nic.beaussart@gmail.com>
2018-06-12 17:26:38 +0200

    removing js in server.js

--- server/src/app/utils.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.uniqFilter = function (x, i, a) { return a.indexOf(x) === i; };
-//# sourceMappingURL=utils.js.map
