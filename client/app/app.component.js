16ef7d236ce06c185d3a52f576602b9e93f35435
Ahder <benzinered@yahoo.fr>
2019-06-01 18:40:22 +0200

    Fix Logout and update profil

--- client/app/app.component.js
+++ client/app/app.component.js
@@ -5,21 +5,42 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var index_1 = require("./_services/index");
 var AppComponent = /** @class */ (function () {
-    function AppComponent() {
+    function AppComponent(userService, authenticationService) {
+        this.userService = userService;
+        this.authenticationService = authenticationService;
+        this.users = [];
     }
     AppComponent.prototype.ngOnInit = function () {
+        this.loadAllUsers();
+        this.currentUser = this.authenticationService.user;
+    };
+    AppComponent.prototype.deleteUser = function (id) {
+        var _this = this;
+        this.userService.delete(id).subscribe(function () { _this.loadAllUsers(); });
+    };
+    AppComponent.prototype.loadAllUsers = function () {
+        var _this = this;
+        this.userService.getAll().subscribe(function (users) { _this.users = users; });
     };
     AppComponent.prototype.getCurrentUser = function () {
+    };
+    AppComponent.prototype.logout = function () {
+        this.authenticationService.logout();
     };
     AppComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'app',
             templateUrl: 'app.component.html'
-        })
+        }),
+        __metadata("design:paramtypes", [index_1.UserService, index_1.AuthenticationService])
     ], AppComponent);
     return AppComponent;
 }());
