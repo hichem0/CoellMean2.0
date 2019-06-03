203d72c65e4927113c5a451dd3b69c69cf68ebf5
hichem <hichembedjaoui4@gmail.com>
2019-06-03 11:19:40 +0200

    add redha changes

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,4 +1,6 @@
 ﻿import {Component, OnInit} from '@angular/core';
+import { User } from './_models/index';
+import { UserService, AuthenticationService } from './_services/index';
 
 @Component({
     moduleId: module.id,
@@ -8,12 +10,31 @@
 
 export class AppComponent implements OnInit{
     private IMAGE_URL: any;
+    currentUser: User;
+    users: User[] = [];
 
-    ngOnInit(): void {
+    constructor(private userService: UserService, private authenticationService: AuthenticationService) {
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+        this.currentUser = this.authenticationService.user;
+    }
+
+    deleteUser(id: string) {
+        this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll().subscribe(users => { this.users = users; });
     }
 
     private getCurrentUser() {
         
+    }
+
+    logout() {
+        this.authenticationService.logout();
     }
 
 }
